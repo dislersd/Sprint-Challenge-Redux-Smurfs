5462e880acfc1e7ae4b3c81f7a0d501b2278e609
dislersd <dislersd@gmail.com>
2019-03-08 08:58:39 -0800

    built form to add smurf

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -3,26 +3,54 @@ import "./App.css";
 import { connect } from "react-redux";
 import { getSmurfs } from "../actions";
 
-
 class App extends Component {
-  state = {};
+  state = {
+    smurf: {
+      name: "",
+      age: "",
+      height: ""
+    }
+  };
 
   componentDidMount() {
-    this.props.getSmurfs()
+    this.props.getSmurfs();
   }
 
   render() {
     return (
       <div className="App">
-      {this.props.smurfs.map( smurf => (
-        <div className='smurf-container' key={smurf.name}>
-          {smurf.name}
-          <hr/>
-          Age: {smurf.age}
-          <hr/>
-          Height: {smurf.height}
-        </div>
-      ))}
+        <form>
+          <input
+            type="text"
+            name="name"
+            value={this.state.smurf.name}
+            placeholder="name..."
+          />
+          <input
+            type="text"
+            name="age"
+            value={this.state.smurf.age}
+            placeholder="age..."
+          />
+          <input
+            type="text"
+            name="height"
+            value={this.state.smurf.height}
+            placeholder="height..."
+          />
+
+          <button> Add Smurf </button>
+        </form>
+
+        {this.props.smurfs.map(smurf => (
+          <div className="smurf-container" key={smurf.name}>
+            {smurf.name}
+            <hr />
+            Age: {smurf.age}
+            <hr />
+            Height: {smurf.height}
+          </div>
+        ))}
       </div>
     );
   }

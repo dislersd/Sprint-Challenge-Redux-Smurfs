6123f3b3f2275b9f632dcd0e14cf12d3b69709e8
dislersd <dislersd@gmail.com>
2019-03-08 08:38:21 -0800

    built out switch cases for fetching smurf data

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,22 +1,42 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
-import {} from '../actions'
+import {
+  FETCH_SMURF_START,
+  FETCH_SMURF_SUCCESS,
+  FETCH_SMURF_FAIL
+} from "../actions";
 
 const initialState = {
-   smurfs: [],
-   fetchingSmurfs: false,
-   addingSmurf: false,
-   updatingSmurf: false,
-   deletingSmurf: false,
-   error: null
- }
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_SMURF_START:
+      return {
+        ...state,
+        error: "",
+        isFetching: true
+      };
+    case FETCH_SMURF_SUCCESS:
+      return {
+        ...state,
+        error: "",
+        isFetching: false,
+        smurfs: action.payload
+      };
+    case FETCH_SMURF_FAIL:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      };
     default:
-    return state;
+      return state;
   }
-}
+};
 
 export default reducer;
